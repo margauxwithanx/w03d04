9e7b63da11802eff4b099c447769c5a42d96d8be
margauxwithanx <margaux@margauxwithanx.com>
2015-10-18 15:02:21 -0700

    initial express server

---
+++ server.js
@@ -0,0 +1,29 @@
+// SERVER-SIDE JAVASCRIPT
+
+// REQUIREMENTS //
+var express = require("express"),
+    app = express(),
+    path = require("path"),
+    bodyParser = require("body-parser");
+
+
+// CONFIG //
+// set ejs as view engine
+app.set('view engine', 'ejs');
+// serve js & css files
+app.use("/static", express.static("public"));
+
+  // from index.ejs: <script src="/static/js/app.js"></script>
+// body parser config to accept our datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', function (req, res) {
+  res.send('Yo homies!');
+})
+
+
+var server = app.listen(process.env.PORT || 3000, function (){
+  console.log("listening on port 3000");
+});
+
